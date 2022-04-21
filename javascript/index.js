588860408dd26090d20c2bbb55e45bf40eef66e8
Jessica Fernandez <jferasef@gmail.com>
2022-04-21 04:53:59 +0200

    steak solved

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
-getInstruction(
+/*getInstruction(
   "mashedPotatoes",
   0,
   (step1) => {
@@ -48,7 +48,7 @@ getInstruction(
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   },
   (error) => console.log(error)
-);
+); */
 
 // Iteration 1 - using callbacks
 getInstruction("mashedPotatoes", 0, (step1) => {
@@ -75,9 +75,57 @@ getInstruction("mashedPotatoes", 0, (step1) => {
 // ...
 
 // Iteration 2 - using promises
+
+obtainInstruction("steak", 0).then((step1) => {
+  document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+  console.log(step1);
+  return obtainInstruction(step1);
+});
+obtainInstruction("steak", 1).then((step2) => {
+  document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+  console.log(step2);
+  return obtainInstruction(step2);
+});
+obtainInstruction("steak", 2).then((step3) => {
+  document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+  console.log(step3);
+  return obtainInstruction(step3);
+});
+obtainInstruction("steak", 3).then((step4) => {
+  document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+  console.log(step4);
+  return obtainInstruction(step4);
+});
+obtainInstruction("steak", 4).then((step5) => {
+  document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+  console.log(step5);
+  return obtainInstruction(step5);
+});
+obtainInstruction("steak", 5).then((step6) => {
+  document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+  console.log(step6);
+  return obtainInstruction(step6);
+});
+obtainInstruction("steak", 6).then((step7) => {
+  document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
+  console.log(step7);
+  return obtainInstruction(step7);
+});
+obtainInstruction("steak", 7)
+  .then((step8) => {
+    document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
+    console.log(step8);
+    return obtainInstruction(step8);
+  })
+  .catch(() => console.error("Something went wrong"));
+
+// ... Your code here
 // ...
 
 // Iteration 3 using async/await
+async function makeBroccoli() {
+  // ... Your code here
+}
 // ...
 
 // Bonus 2 - Promise all
