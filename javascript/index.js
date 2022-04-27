64e8b8256c44ec2d5361e383be814bcbb43fcc5d
Jessica Fernandez <jferasef@gmail.com>
2022-04-27 20:21:35 +0200

    solved-lab using promises fixed

--- javascript/index.js
+++ javascript/index.js
@@ -78,47 +78,46 @@ getInstruction("mashedPotatoes", 0, (step1) => {
 // ...
 
 // Iteration 2 - using promises
-
-obtainInstruction("steak", 0).then((step1) => {
-  document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
-  console.log(step1);
-  return obtainInstruction(step1);
-});
-obtainInstruction("steak", 1).then((step2) => {
-  document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
-  console.log(step2);
-  return obtainInstruction(step2);
-});
-obtainInstruction("steak", 2).then((step3) => {
-  document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
-  console.log(step3);
-  return obtainInstruction(step3);
-});
-obtainInstruction("steak", 3).then((step4) => {
-  document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
-  console.log(step4);
-  return obtainInstruction(step4);
-});
-obtainInstruction("steak", 4).then((step5) => {
-  document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
-  console.log(step5);
-  return obtainInstruction(step5);
-});
-obtainInstruction("steak", 5).then((step6) => {
-  document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
-  console.log(step6);
-  return obtainInstruction(step6);
-});
-obtainInstruction("steak", 6).then((step7) => {
-  document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
-  console.log(step7);
-  return obtainInstruction(step7);
-});
-obtainInstruction("steak", 7)
+obtainInstruction("steak", 0)
+  .then((step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+    console.log(step1);
+    return obtainInstruction("steak", 1);
+  })
+  .then((step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+    console.log(step2);
+    return obtainInstruction("steak", 2);
+  })
+  .then((step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+    console.log(step3);
+    return obtainInstruction("steak", 3);
+  })
+  .then((step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+    console.log(step4);
+    return obtainInstruction("steak", 4);
+  })
+  .then((step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+    console.log(step5);
+    return obtainInstruction("steak", 5);
+  })
+  .then((step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+    console.log(step6);
+    return obtainInstruction("steak", 6);
+  })
+  .then((step7) => {
+    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
+    console.log(step7);
+    return obtainInstruction("steak", 7);
+  })
   .then((step8) => {
     document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
     document.querySelector("#steakImg").removeAttribute("hidden");
-    console.log(step8);
+    console.log("steak", 8);
     return obtainInstruction(step8);
   })
   .catch(() => console.error("Something went wrong"));
@@ -155,23 +154,23 @@ makeBroccoli();
 
 // Bonus 2 - Promise all
 const prom1 = obtainInstruction("brusselsSprouts", 0);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom1}</li>`;
+
 const prom2 = obtainInstruction("brusselsSprouts", 1);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom2}</li>`;
+
 const prom3 = obtainInstruction("brusselsSprouts", 2);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom3}</li>`;
+
 const prom4 = obtainInstruction("brusselsSprouts", 3);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom4}</li>`;
+
 const prom5 = obtainInstruction("brusselsSprouts", 4);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom5}</li>`;
+
 const prom6 = obtainInstruction("brusselsSprouts", 5);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom6}</li>`;
+
 const prom7 = obtainInstruction("brusselsSprouts", 6);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom7}</li>`;
+
 const prom8 = obtainInstruction("brusselsSprouts", 7);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom8}</li>`;
+
 const prom9 = obtainInstruction("brusselsSprouts", 7);
-//document.querySelector("#brusselsSprouts").innerHTML += `<li>${prom9}</li>`;
+
 document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
 
 Promise.all([prom1, prom2, prom3, prom4, prom5, prom6, prom7, prom8, prom9])
